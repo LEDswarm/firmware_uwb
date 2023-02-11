c214a158a8ec15e4511d82c989f1c3a1962e9fd6
Moritz Riede <moritz@mriede.com>
2023-02-11 17:45:42 +0100

    feat: start working on driving the display

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,33 @@ use std::{
     thread::sleep,
     time::Duration,
 };
-use esp_idf_hal::{
-    peripherals::Peripherals,
-};
 use esp_idf_svc::{
     wifi::EspWifi,
     nvs::EspDefaultNvsPartition,
     eventloop::EspSystemEventLoop,
 };
 use embedded_svc::wifi::{ClientConfiguration, Wifi, Configuration};
+use esp_idf_hal::delay::{FreeRtos, BLOCK};
+use esp_idf_hal::i2c::*;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::prelude::*;
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use embedded_graphics::{
+    image::{Image, ImageRaw},
+    pixelcolor::BinaryColor,
+    prelude::*,
+};
 
-fn main(){
+const SSD1306_ADDRESS: u8 = 0x3c;
+
+fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();//Needed for esp32-rs
     println!("Entered Main function!");
     let peripherals = Peripherals::take().unwrap();
     let sys_loop = EspSystemEventLoop::take().unwrap();
     let nvs = EspDefaultNvsPartition::take().unwrap();
 
-    let mut wifi_driver = EspWifi::new(
+    /*let mut wifi_driver = EspWifi::new(
         peripherals.modem,
         sys_loop,
         Some(nvs)
@@ -38,10 +47,33 @@ fn main(){
         let config = wifi_driver.get_configuration().unwrap();
         println!("Waiting for station {:?}", config);
     }
-    println!("Should be connected now");
-    loop{
+    println!("Should be connected now");*/
+
+    let i2c = peripherals.i2c0;
+    let sda = peripherals.pins.gpio21;
+    let scl = peripherals.pins.gpio22;
+
+    let config = I2cConfig::new().baudrate(100.kHz().into());
+    let mut i2c = I2cDriver::new(i2c, sda, scl, &config)?;
+
+
+    /*loop{
         println!("IP info: {:?}", wifi_driver.sta_netif().get_ip_info().unwrap());
         sleep(Duration::new(10,0));
-    }
+    }*/
 
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("./rust.raw"), 64);
+
+    let im = Image::new(&raw, Point::new(32, 0));
+
+    im.draw(&mut display).unwrap();
+
+    display.flush().unwrap();
+
+    loop {}
 }
