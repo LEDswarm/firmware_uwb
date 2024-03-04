5f8755fd30b617713e5556bfec9bed891962c33b
Moritz Riede <moritz@mriede.com>
2024-03-04 02:08:09 +0100

    chore: use new `InternalMessage` enum

--- src/imu.rs
+++ src/imu.rs
@@ -5,13 +5,13 @@ use std::{time::Duration, sync::mpsc::Sender};
 use adxl343::accelerometer::Accelerometer;
 use esp_idf_hal::{i2c::{I2C0, I2cConfig, I2cDriver}, gpio::AnyIOPin};
 use esp_idf_svc::sys::EspError;
-use ledswarm_protocol::{Message, Notice};
+use ledswarm_protocol::{InternalMessage, Message, Notice};
 use esp_idf_svc::hal::gpio::{Gpio21, Gpio22};
 use esp_idf_svc::hal::prelude::*;
 
 use crate::moving_average;
 
-pub fn start(tx: Sender<Message>, i2c: I2C0, sda: Gpio21, scl: Gpio22) -> Result<(), EspError> {
+pub fn start(tx: Sender<InternalMessage>, i2c: I2C0, sda: Gpio21, scl: Gpio22) -> Result<(), EspError> {
     std::thread::spawn(move || {
         let config = I2cConfig::new().baudrate(100.kHz().into());
         let i2c = I2cDriver::new(i2c, sda, scl, &config).unwrap();
@@ -24,7 +24,7 @@ pub fn start(tx: Sender<Message>, i2c: I2C0, sda: Gpio21, scl: Gpio22) -> Result
             moving_average.add(reading);
             let delta = moving_average.get_average_delta();
             // The receiving end in the controller MUST handle these events or the buffer overflow will cause an out-of-memory error after about 15 seconds.
-            tx.send(Message::Notice(Notice::Accelerometer(delta))).unwrap();
+            tx.send(InternalMessage::AccelerometerDelta(delta)).unwrap();
             std::thread::sleep(Duration::from_millis(2));
         }
     });
