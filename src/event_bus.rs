130760ac04b5e78cb0146f1fa1d0fa77cc09eb59
Moritz Riede <moritz@mriede.com>
2024-03-04 02:09:32 +0100

    chore: start working on event bus infrastructure

---
+++ src/event_bus.rs
@@ -0,0 +1,34 @@
+use async_channel::{unbounded, Sender, Receiver};
+use futures::executor::block_on;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use ledswarm_protocol::InternalMessage;
+
+pub struct EventBus {
+    subscribers: Arc<Mutex<HashMap<String, Vec<Sender<(String, InternalMessage)>>>>>,
+}
+
+impl EventBus {
+    pub fn new() -> Self {
+        EventBus {
+            subscribers: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    async fn subscribe(&self, tag: &str) -> Receiver<(String, InternalMessage)> {
+        let (tx, rx) = unbounded();
+        let mut subs = self.subscribers.lock().unwrap();
+        subs.entry(tag.to_string()).or_default().push(tx);
+        rx
+    }
+
+    async fn publish(&self, tag: &str, event: InternalMessage) {
+        let subs = self.subscribers.lock().unwrap();
+        if let Some(subscribers) = subs.get(tag) {
+            for tx in subscribers {
+                let _ = tx.send((tag.to_string(), event.clone())).await;
+            }
+        }
+    }
+}
